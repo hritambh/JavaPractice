1ab407a5fbd94bcd8af04fe54dc4d9f83f425be6
hritambh <hritambh9@gmail.com>
2024-10-14 19:58:25 +0530

    LongestPalindromicSubstring

--- src/DSA/DP/DP_1D/LongestPalindromicSubstring.java
+++ src/DSA/DP/DP_1D/LongestPalindromicSubstring.java
@@ -10,6 +10,35 @@ Output: "bab"
 
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-        String s = "ababd";
+        String s = "forgeeksskeegfor";
+//        if (checkPal(s,0,s.length()-1))
+//            System.out.println("Is Palindrome");
+//        else
+//            System.out.println("Not Palindrome");
+        System.out.println(longestPalSubstr(s));
+    }
+
+    private static String longestPalSubstr(String s) {
+        int n = s.length();
+        int maxLen = 1, start = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkPal(s,i,j) && (j-i+1)>maxLen){
+                    start = i;
+                    maxLen = j-i+1;
+                }
+            }
+        }
+        return s.substring(start,start+maxLen);
+    }
+
+    private static boolean checkPal(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low) != s.charAt(high))
+                return false;
+            low++;
+            high--;
+        }
+        return true;
     }
 }
