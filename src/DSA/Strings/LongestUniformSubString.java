9e9c95f80c59d2f86a5c08bef813d923e9ce1b0c
hritambh <hritambh9@gmail.com>
2025-06-01 20:08:08 +0530

    additional changes

--- src/DSA/Strings/LongestUniformSubString.java
+++ src/DSA/Strings/LongestUniformSubString.java
@@ -4,31 +4,6 @@ import java.util.List;
 
 public class LongestUniformSubString {
     public static void main(String[] args) {
-        String s = "aabbbbcde";
-        System.out.println(longest(s));
-    }
-
-    private static List<Integer> longest(String s) {
-        int n = s.length();
-        int maxCount = 0;
-        int index = 0;
-        int i =0;
-        while (i<n){
-            int j = i;
-
-            while (j<n && s.charAt(j)==s.charAt(i))
-                j++;
-
-            int len = j-i;
-
-            if (len>maxCount){
-                maxCount= len;
-                index = i;
-            }
-
-            i=j;
-        }
-
-        return List.of(index,maxCount);
+        System.out.println("Hello");
     }
 }
